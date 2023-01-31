96dc8627be2d09ab9c7b49e095202a9f2de61720
Abu Hanif Muhammad Syarubany <73388705+hanifsyarubany@users.noreply.github.com>
2023-01-31 07:56:40 +0700

    Using Cohere

--- src/helper_nlp.py
+++ src/helper_nlp.py
@@ -14,41 +14,43 @@ load_dotenv()
 
 class NLPClient:
     def __init__(self):
-        self.get_nlp_client()
+        self.co = self.get_nlp_client()
 
     
     def get_nlp_client(self):
-        openai.api_key = os.environ.get('OPEN_API_KEY')
+        return cohere.Client('COHERE_API_KEY')
     
     
     def open_file(self,filepath):
         with open(filepath,'r',encoding='utf-8') as infile:
             return infile.read()
     
-    def gpt3_completion(self,prompt, engine='text-davinci-003',temp=1.1,top_p=1.0,tokens=2000,freq_pen=0.0,pres_pen=0.0,stop=['USER:']):
+    def cohere_completion(prompt, engine='command-xlarge-20221108',, temp=0.0, top_k=0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:']):
         max_retry = 5
         retry = 0
         prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
         while True:
             try:
-                response = openai.Completion.create(
-                    engine = engine,
-                    prompt = prompt,
+                response = self.co.generate(
+                    model=engine,
+                    prompt=prompt,
+                    max_tokens=tokens,
                     temperature=temp,
-                    max_tokens = tokens,
-                    top_p = top_p,
-                    frequency_penalty = freq_pen,
-                    presence_penalty = pres_pen,
-                    stop = stop)
-                text = response['choices'][0]['text'].strip()
+                    k=top_k,
+                    p=top_p,
+                    frequency_penalty=freq_pen,
+                    presence_penalty=pres_pen,
+                    stop_sequences=stop,
+                    return_likelihoods='NONE')
+                text = response.generations[0].text.strip()
                 text = re.sub('\s+',' ',text)
                 return text 
 
             except Exception as oops:
                 retry += 1
                 if retry >= max_retry:
-                    return 'GPT3 error: %s' % oops
-                print('Error communicating with OpenAI:',oops)
+                    return 'Cohere error: %s' % oops
+                print('Error communicating with Cohere:',oops)
                 exit()
                 sleep(1)
 
@@ -56,6 +58,6 @@ class NLPClient:
     def get_completion(self,block):
         prompt = self.open_file('../prompt_senti.txt').replace('<<CONVERSATION>>',block)
         logger.debug(f'The final prompt! \n {prompt}')
-        response = self.gpt3_completion(prompt)
+        response = self.cohere_completion(prompt)
         logger.debug(f'The final response from bot: {response}')
         return response
